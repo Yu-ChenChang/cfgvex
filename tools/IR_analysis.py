644bdf3e8cc092620bd774687482bb69b784e9f9
Yu-Chen Chang <chang397@purdue.edu>
2016-03-05 14:43:52 -0500

    Complete extract uninitialized memory usage

--- tools/IR_analysis.py
+++ tools/IR_analysis.py
@@ -14,30 +14,26 @@ def __findIRtype(leftside,rightside):
 	if "GET" in rightside:
 		if "I32" in rightside or "I64" in rightside:
 			target = rightside.partition('(')[-1].rpartition(')')[0]
-			var = leftside.strip()
+			varName = leftside.strip()
 			#print target
-			return (var,[target,IRTYPE.GET])
+			return (varName,[target,IRTYPE.GET])
 	
 	
 	elif "Add" in rightside:
 		if "Add32" in rightside or "Add64" in rightside:
 			target = rightside.partition('(')[-1].rpartition(',')[0]
-			target2 = rightside.partition(',')[-1].rpartition(')')[0][2:]
-			if '0x' in target2:
-				target2 = struct.unpack('>i', rightside.partition(',')[-1].rpartition(')')[0][2:].decode('hex'))[0]
-			var = leftside.strip()
+			target2 = rightside.partition(',')[-1].rpartition(')')[0]
+			varName = leftside.strip()
 			#print target
-			return (var,[target,IRTYPE.Add,target2])
+			return (varName,[target,IRTYPE.Add,target2])
 
 	elif "Sub" in rightside:
 		if "Sub32" in rightside or "Sub64" in rightside:
 			target = rightside.partition('(')[-1].rpartition(',')[0]
-			target2 = rightside.partition(',')[-1].rpartition(')')[0][2:]
-			if '0x' in target2:
-				target2 = struct.unpack('>i', rightside.partition(',')[-1].rpartition(')')[0][2:].decode('hex'))[0]
-			var = leftside.strip()
+			target2 = rightside.partition(',')[-1].rpartition(')')[0]
+			varName = leftside.strip()
 			#print target
-			return (var,[target,IRTYPE.Add,target2])
+			return (varName,[target,IRTYPE.Sub,target2])
 
 	elif "Shl" in rightside:
 		if "Shl32" in rightside or "Shl64" in rightside:
@@ -45,43 +41,65 @@ def __findIRtype(leftside,rightside):
 			target2 = rightside.partition(',')[-1].rpartition(')')[0][2:]
 			if '0x' in target2:
 				target2 = struct.unpack('>i', rightside.partition(',')[-1].rpartition(')')[0][2:].decode('hex'))[0]
-			var = leftside.strip()
+			varName = leftside.strip()
 			#print target
-			return (var,[target,IRTYPE.Shl,target2])
+			return (varName,[target,IRTYPE.Shl,target2])
 
 	elif "LD" in rightside:
 		if "LDle" in rightside:
 			target = rightside.partition('(')[-1].rpartition(')')[0]
-			var = leftside.strip()
+			varName = leftside.strip()
 			#print target
-			return (var,[target,IRTYPE.LD])
+			return (varName,[target,IRTYPE.LD])
 
-	elif 't' in leftside and ('t' in rightside or '0x' in rightside):
-		target = leftside.strip()
-		var = rightside.strip()
+	## simulated as Ass ##
+	elif "F32toF64" in rightside:
+		target = rightside.partition('(')[-1].rpartition(')')[0]
+		varName = leftside.strip()
+		print target
 		#print target
-		return (var,[target,IRTYPE.Ass])
+		return (varName,[target,IRTYPE.Ass])
+
+	elif ('t' in rightside or '0x' in rightside) and 't' in leftside:
+		target = rightside.strip()
+		varName = leftside.strip()
+		#print target
+		return (varName,[target,IRTYPE.Ass])
+
 
 ## leftside operator ##
 	if "PUT" in leftside:
 		target = leftside.partition('(')[-1].rpartition(')')[0]
-		var = rightside.strip()
+		varName = rightside.strip()
 		#print target
-		return (var,[target,IRTYPE.PUT])
+		return (varName,[target,IRTYPE.PUT])
 
 	elif "ST" in leftside:
 		if "STle" in side:
 			target = side.partition('(')[-1].rpartition(')')[0]
-			var = rightside.strip()
+			varName = rightside.strip()
 			#print target
-			return (var,[target,IRTYPE.ST])
+			return (varName,[target,IRTYPE.ST])
 
-## return a tuple (read target, uninitialized) ##
+def tvarToExp(tvar, varName):
+	if tvar[varName][1] > 0:
+		memory = tvar[varName][0] + '_add_' + hex(tvar[varName][1])
+	elif tvar[varName][1] == 0:
+		memory = tvar[varName][0]
+	else:
+		memory = tvar[varName][0] + '_sub_' + hex(abs(tvar[varName][1]))
+	return memory
+
+## return uninitialized register or memory location that has been read ##
 def analysisIR(inst_ir,initList):
+	uniList = []
+	tvar = {}
+	filtered = ['if','x86g_calculate_condition','32to1']
+
 	print inst_ir.split('\n')[0]
-	filtered = ['if','x86g_calculate_condition','32to1','F32toF64']
 	for line in inst_ir.split('\n'):
 		print "-----ir start-----"
+		print line
 		filflag = False
 		if any(irst in line for irst in filtered):
 			continue
@@ -92,16 +110,62 @@ def analysisIR(inst_ir,initList):
 		leftside = line[:ind].strip()
 		rightside = line[ind+1:].strip()
 		try:
-			L,R = __findIRtype(leftside,rightside)
+			varName,IRinfo = __findIRtype(leftside,rightside)
 		except:
 			error_exit("Err: Found undefined IR Type in \"%s\"" %line)
-		print L
-		print R
 		
+		## combine each IR inst result ##
+		if IRinfo[1] == IRTYPE.GET:
+			tvar[varName] = (IRinfo[0],0)
 
+		elif IRinfo[1] == IRTYPE.Add:
+			if '0x' in IRinfo[2]:
+				tvar[varName] = (tvar[IRinfo[0]][0] , tvar[IRinfo[0]][1] + struct.unpack('>i', IRinfo[2][2:].decode('hex'))[0])
+			else:
+				tvar[varName] = (tvar[IRinfo[0]][0] , tvar[IRinfo[0]][1] + tvar[IRinfo[2]][1])
 
+		elif IRinfo[1] == IRTYPE.Sub:
+			if '0x' in IRinfo[2]:
+				tvar[varName] = (tvar[IRinfo[0]][0] , tvar[IRinfo[0]][1] - struct.unpack('>i', IRinfo[2][2:].decode('hex'))[0])
+			else:
+				tvar[varName] = (tvar[IRinfo[0]][0] , tvar[IRinfo[0]][1] - tvar[IRinfo[2]][1])
 
-	return []
+		## simulated as Ass ##
+		elif IRinfo[1] == IRTYPE.Shl:
+			tvar[varName] = tvar[IRinfo[0]]
+
+		elif IRinfo[1] == IRTYPE.LD:
+			tvar[varName] = (tvar[IRinfo[0]][0] , tvar[IRinfo[0]][1])
+
+		elif IRinfo[1] == IRTYPE.Ass:
+			if '0x' in IRinfo[0]:
+				tvar[varName] = ('memory' , int(IRinfo[0][2:],16))
+			else:
+				tvar[varName] = tvar[IRinfo[0]]
+
+## leftside operator ##
+		elif IRinfo[1] == IRTYPE.PUT:
+			if '0x' not in varName:
+				if tvar[varName][0] == 'memory':
+					continue
+				memory = tvarToExp(tvar, varName)
+
+				## If the register is not updating itself (like esp = esp-4) ##
+				if memory not in initList and tvar[varName][0] != IRinfo[0]:
+					if memory not in uniList:
+						uniList += [memory]
+			if IRinfo[0] not in initList:
+				initList += [IRinfo[0]]
+
+		elif IRinfo[1] == IRTYPE.ST:
+			memory = tvarToExp(tvar, varName)
+			if memory not in initList and memory not in uniList:
+				uniList += [memory]
+			if IRinfo[0] not in initList:
+				initList += [IRinfo[0]]
+		print tvar
+		print uniList	
+	return uniList
 	
 
 	
--- tools/para_finder.py
+++ tools/para_finder.py
@@ -15,7 +15,7 @@ class paraFinder:
 		for block in func.blocks:
 			self.b_status[block.base_address] = False
 			self.addr_to_block[block.base_address] = block
-		self.analysisBlock(func.blocks[0],[])
+		self.analysisBlock(func.blocks[0],['esp','ebp'])
 
 	def analysisBlock(self,block,initList):
 		curList = initList
@@ -27,8 +27,10 @@ class paraFinder:
 				self.analysisBlock(self.addr_to_block[follow_b],curList)
 
 	def analysisInst(self,inst_ir,initList):
+		print "initList=============="
+		print initList
 		target = analysisIR(inst_ir,initList)
-		self.unint_para += target
+		self.unint_para.extend( x for x in target if x not in self.unint_para)
 
 	def getParaNum(self):
 		print self.unint_para
