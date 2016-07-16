2cafa2505baabed20118711a125744a3a21f279d
Yu-Chen Chang <chang397@purdue.edu>
2016-07-15 21:10:59 -0400

    funcname with number of arguments

--- analysis/readelf/__init__.py
+++ analysis/readelf/__init__.py
@@ -69,7 +69,7 @@ class ReadElf(object):
 		else:
 			self._dwarfinfo = None
 
-	def display_debug_dump(self):
+	def get_func_with_params(self):
 		""" Dump the debugging info section.
 		"""
 		self._init_dwarfinfo()
@@ -78,97 +78,33 @@ class ReadElf(object):
 		section_offset = self._dwarfinfo.debug_info_sec.global_offset
 		# Offset of the .debug_info section in the stream
 		for cu in self._dwarfinfo.iter_CUs():
-			self._emitline('  Compilation Unit @ offset %s:' %
-				self._format_hex(cu.cu_offset))
-			self._emitline('   Length:		  %s (%s)' % (
-				self._format_hex(cu['unit_length']),
-				'%s-bit' % cu.dwarf_format()))
-			self._emitline('   Version:		  %s' % cu['version']),
-			self._emitline('   Abbrev Offset: %s' % (
-				self._format_hex(cu['debug_abbrev_offset']))),
-			self._emitline('   Pointer Size:  %s' % cu['address_size'])
-
-			# The nesting depth of each DIE within the tree of DIEs must be
-			# displayed. To implement this, a counter is incremented each time
-			# the current DIE has children, and decremented when a null die is
-			# encountered. Due to the way the DIE tree is serialized, this will
-			# correctly reflect the nesting depth
-			#
-			die_depth = 0
+			funcDic = {} 
+			paraCount = 0
+			funcName=""
 			for die in cu.iter_DIEs():
-				self._emitline(' <%s><%x>: Abbrev Number: %s%s' % (
-					die_depth,
-					die.offset,
-					die.abbrev_code,
-					(' (%s)' % die.tag) if not die.is_null() else ''))
-				if die.is_null():
-					die_depth -= 1
-					continue
+				if die.tag == 'DW_TAG_subprogram':
+					if funcName != "":
+						funcDic[funcName] = paraCount
+						paraCount=0
+					for i,attr in enumerate(itervalues(die.attributes)):
+						if i==1:
+							funcName=describe_attr_value(attr, die, section_offset).split()[-1]
 
-				for attr in itervalues(die.attributes):
-					name = attr.name
-					# Unknown attribute values are passed-through as integers
-					if isinstance(name, int):
-						name = 'Unknown AT value: %x' % name
-					self._emitline('	<%x>   %-18s: %s' % (
-						attr.offset,
-						name,
-						describe_attr_value(
-							attr, die, section_offset)))
-
-				if die.has_children:
-					die_depth += 1
-
-		self._emitline()
-
-	def _format_hex(self, addr, fieldsize=None, fullhex=False, lead0x=True,
-					alternate=False):
-		""" Format an address into a hexadecimal string.
-
-			fieldsize:
-				Size of the hexadecimal field (with leading zeros to fit the
-				address into. For example with fieldsize=8, the format will
-				be %08x
-				If None, the minimal required field size will be used.
-
-			fullhex:
-				If True, override fieldsize to set it to the maximal size
-				needed for the elfclass
-
-			lead0x:
-				If True, leading 0x is added
-
-			alternate:
-				If True, override lead0x to emulate the alternate
-				hexadecimal form specified in format string with the #
-				character: only non-zero values are prefixed with 0x.
-				This form is used by readelf.
-		"""
-		if alternate:
-			if addr == 0:
-				lead0x = False
-			else:
-				lead0x = True
-				fieldsize -= 2
-
-		s = '0x' if lead0x else ''
-		if fullhex:
-			fieldsize = 8 if self.elffile.elfclass == 32 else 16
-		if fieldsize is None:
-			field = '%x'
-		else:
-			field = '%' + '0%sx' % fieldsize
-		return s + field % addr
-
-	def _emitline(self, s=''):
-		""" Emit an object to output, followed by a newline
-		"""
-		self.output.write(str(s).rstrip() + '\n')
+				elif die.tag == 'DW_TAG_formal_parameter':
+					paraCount+=1
+			if funcName != "":
+				funcDic[funcName] = paraCount
+		for ele in funcDic:
+			print '%s: %d' % (
+				ele,
+				funcDic[ele]
+				)
+		return funcDic
 def program_dwarf(pName):
 	with open(pName, 'rb') as file:
 		try:
 			readelf = ReadElf(file, sys.stdout)
-			readelf.display_debug_dump()
+			funcDic = readelf.get_func_with_params()
 		except ELFError as ex:
 			sys.stderr.write('ELF error: %s\n' % ex)
 			sys.exit(1)
